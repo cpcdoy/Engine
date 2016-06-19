bb49de8ca276425d7715600419f490f834fa5199
Chady Dimachkie <dimach_c@epita.fr>
2016-06-19 13:42:55 +0200

    Add: debug manager

---
+++ src/debug/debug_manager.cc
@@ -0,0 +1,58 @@
+#include "debug_manager.hh"
+
+namespace debug
+{
+  debug_manager::debug_manager(std::string path)
+  {
+    handle = dlopen(path.c_str(), RTLD_LAZY);
+
+    if (!handle)
+      std::cerr << "Error: cannot find/open the debugger's shared object" << std::endl;
+    else
+    {
+      RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(handle, "RENDERDOC_GetAPI");
+
+      assert(RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_0_0, (void **)&rdoc_api) == 1);
+
+      assert(rdoc_api->StartFrameCapture != NULL && rdoc_api->EndFrameCapture != NULL);
+
+      int major = 999;
+      int minor = 999;
+      int patch = 999;
+
+      rdoc_api->GetAPIVersion(&major, &minor, &patch);
+
+      assert(major == 1 && minor >= 0 && patch >= 0);
+
+      rdoc_api->SetLogFilePathTemplate("capture_logs");
+
+      rdoc_api->SetFocusToggleKeys(NULL, 0);
+
+      RENDERDOC_InputButton captureKey = eRENDERDOC_Key_F11;
+      rdoc_api->SetCaptureKeys(&captureKey, 1);
+
+      rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_AllowVSync,      1);
+      rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_SaveAllInitials, 1);
+
+      rdoc_api->MaskOverlayBits(eRENDERDOC_Overlay_None, eRENDERDOC_Overlay_None);
+    }
+  }
+
+  debug_manager::debug_manager()
+    : debug_manager::debug_manager(std::string("libs/librenderdoc.so"))
+  {
+  }
+
+  void debug_manager::trigger_capture()
+  {
+    if (handle)
+      rdoc_api->TriggerCapture();
+    else
+      std::cerr << "Error: cannot trigger a capture because the API isn't loaded" << std::endl;
+  }
+
+  debug_manager::~debug_manager()
+  {
+    dlclose(handle);
+  }
+}
---
+++ src/debug/debug_manager.hh
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cassert>
+#include <dlfcn.h>
+#include "renderdoc_app.h"
+
+namespace debug
+{
+  class debug_manager
+  {
+    public:
+      debug_manager();
+      ~debug_manager();
+      debug_manager(std::string path);
+
+      void trigger_capture();
+    private:
+      void* handle;
+      RENDERDOC_API_1_0_0 *rdoc_api = nullptr;
+      pRENDERDOC_GetAPI RENDERDOC_GetAPI;
+  };
+}
