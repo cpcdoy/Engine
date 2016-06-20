5811211126cd011b5181564a91d915c8791d893f
Chady Dimachkie <dimach_c@epita.fr>
2016-06-20 02:46:27 +0200

    Fix: logger calling

--- src/debug/debug_manager.cc
+++ src/debug/debug_manager.cc
@@ -5,40 +5,48 @@ namespace debug
 {
   debug_manager::debug_manager(std::string path)
   {
-    Log().Get(LogLevel::logINFO) << "Loading the debug manager"
+    log::get(log_level::logINFO) << "Loading the debug manager"
                                  << std::endl;
     handle = dlopen(path.c_str(), RTLD_LAZY);
 
     if (!handle)
-      Log().Get(LogLevel::logERROR) << "Cannot find/open the debugger's shared object"
+      log::get(log_level::logERROR) << "Cannot find/open the debugger's shared object"
                                            << std::endl;
     else
     {
       RENDERDOC_GetAPI = (pRENDERDOC_GetAPI)dlsym(handle, "RENDERDOC_GetAPI");
 
-      assert(RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_0_0, (void **)&rdoc_api) == 1);
+      if (!RENDERDOC_GetAPI)
+      {
+        log::get(log_level::logERROR) << "Cannot find symbol RENDERDOC_GetAPI in " << path
+          << std::endl;
+      }
+      else
+      {
+        assert(RENDERDOC_GetAPI(eRENDERDOC_API_Version_1_0_0, (void **)&rdoc_api) == 1);
 
-      assert(rdoc_api->StartFrameCapture != NULL && rdoc_api->EndFrameCapture != NULL);
+        assert(rdoc_api->StartFrameCapture != NULL && rdoc_api->EndFrameCapture != NULL);
 
-      int major = 999;
-      int minor = 999;
-      int patch = 999;
+        int major = 999;
+        int minor = 999;
+        int patch = 999;
 
-      rdoc_api->GetAPIVersion(&major, &minor, &patch);
+        rdoc_api->GetAPIVersion(&major, &minor, &patch);
 
-      assert(major == 1 && minor >= 0 && patch >= 0);
+        assert(major == 1 && minor >= 0 && patch >= 0);
 
-      rdoc_api->SetLogFilePathTemplate("capture_logs");
+        rdoc_api->SetLogFilePathTemplate("capture_logs");
 
-      rdoc_api->SetFocusToggleKeys(NULL, 0);
+        rdoc_api->SetFocusToggleKeys(NULL, 0);
 
-      RENDERDOC_InputButton captureKey = eRENDERDOC_Key_F11;
-      rdoc_api->SetCaptureKeys(&captureKey, 1);
+        RENDERDOC_InputButton captureKey = eRENDERDOC_Key_F11;
+        rdoc_api->SetCaptureKeys(&captureKey, 1);
 
-      rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_AllowVSync, 1);
-      rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_SaveAllInitials, 1);
+        rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_AllowVSync, 1);
+        rdoc_api->SetCaptureOptionU32(eRENDERDOC_Option_SaveAllInitials, 1);
 
-      rdoc_api->MaskOverlayBits(eRENDERDOC_Overlay_None, eRENDERDOC_Overlay_None);
+        rdoc_api->MaskOverlayBits(eRENDERDOC_Overlay_None, eRENDERDOC_Overlay_None);
+      }
     }
   }
 
@@ -47,12 +55,13 @@ namespace debug
     if (handle)
     {
       rdoc_api->TriggerCapture();
-      Log().Get(LogLevel::logDEBUG) << "Triggered frame capture (to be saved in ./"
-                                    << rdoc_api->GetLogFilePathTemplate() << ")"
-                                    << std::endl;
+      log::get(log_level::logDEBUG) << "Triggered frame capture" << std::endl;
+      log::get(log_level::logINDENT) << "To be saved in ./"
+                                     << rdoc_api->GetLogFilePathTemplate()
+                                     << std::endl;
     }
     else
-      Log().Get(LogLevel::logERROR) << "Error: cannot trigger a capture because the debug API isn't loaded"
+      log::get(log_level::logERROR) << "Error: cannot trigger a capture because the debug API isn't loaded"
                                            << std::endl;
   }
 
@@ -62,7 +71,7 @@ namespace debug
     {
       rdoc_api->Shutdown();
       dlclose(handle);
-      Log().Get(LogLevel::logDEBUG) << "Shutting down the debug API"
+      log::get(log_level::logDEBUG) << "Shutting down the debug API"
         << std::endl;
     }
   }
