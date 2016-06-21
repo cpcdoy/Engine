1ed70daacce1502b63d50cfdefcadc1846095549
Chady Dimachkie <dimach_c@epita.fr>
2016-06-21 23:14:13 +0200

    Add: using shader_ptr for the generated mesh

--- src/resource/obj_loader.hh
+++ src/resource/obj_loader.hh
@@ -4,8 +4,12 @@
 # include <string>
 # include <cstring>
 # include <iostream>
+# include <iomanip>
+# include <memory>
 # include <glm/glm.hpp>
+
 # include "loader.hh"
+# include "mesh.hh"
 
 namespace resource
 {
@@ -13,15 +17,21 @@ namespace resource
   {
     public:
       obj_loader();
-      ~obj_loader();
-      
+      virtual ~obj_loader() override;
+
       void load_obj(const char* path);
-      virtual void load(const char* path) override;
+      void clean_load();
+      virtual bool load(const char* path) override;
+      virtual std::shared_ptr<mesh> generate_mesh() override;
 
     private:
       std::vector<unsigned int> vertex_indices, uv_indices, normal_indices;
       std::vector<glm::vec3> temp_vertices;
       std::vector<glm::vec2> temp_uvs;
       std::vector<glm::vec3> temp_normals;
+
+      std::vector<glm::vec3> out_vertices;
+      std::vector<glm::vec2> out_uvs;
+      std::vector<glm::vec3> out_normals;
   };
 }
