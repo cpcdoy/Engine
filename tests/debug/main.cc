9f8be418070820c3fe3511b0531d99eecd45f846
Chady Dimachkie <dimach_c@epita.fr>
2016-06-21 23:17:15 +0200

    Add: test for the resource_manager

--- tests/debug/main.cc
+++ tests/debug/main.cc
@@ -1,14 +1,21 @@
 #include "../../src/debug/debug_manager.hh"
-#include "../../src/resource/obj_loader.hh"
+#include "../../src/resource/resource_manager.hh"
 
 int main(void)
 {
   debug::debug_manager dm;
   dm.trigger_capture();
 
-  resource::obj_loader obj_loader;
+  /*resource::obj_loader obj_loader;
   obj_loader.load_obj("tests/res/test.obj");
-  obj_loader.load_obj("lol.obj");
+  obj_loader.generate_mesh();
+
+  obj_loader.load_obj("lol.obj");*/
+
+  resource::resource_manager rm;
+
+  rm.load("tests/res/test.obj");
+  rm.load("lol.obj");
 
   return 0;
 }
