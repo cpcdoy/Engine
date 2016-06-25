0c5981c040aa69893f728acb1ddbca0d2efb70db
Chady Dimachkie <dimach_c@epita.fr>
2016-06-26 00:44:15 +0200

    Add: ui_manager now inherits from the base_manager

--- src/ui/ui_manager.cc
+++ src/ui/ui_manager.cc
@@ -4,25 +4,25 @@ namespace ui
 {
   ui_manager::ui_manager()
   {
-    uis.push_back(std::make_shared<glfw_ui>());
+    managees.push_back(std::make_shared<glfw_ui>());
   }
 
   ui_manager::~ui_manager()
   {
-    uis.clear();
+    managees.clear();
   }
 
   bool ui_manager::init_ui()
   {
     debug::log::get(debug::logINFO) << "Loading UI system" << std::endl;
 
-    for (auto i = uis.begin(); i != uis.end(); i++)
+    for (auto i = managees.begin(); i != managees.end(); i++)
     {
       debug::log::get(debug::logINDENT, 5) << "Trying UI system \"" << (*i)->get_base_ui_id()
                                            << "\"" << std::endl;
       if ((*i)->init_base_ui())
       {
-        current_ui = *i;
+        current_managee = *i;
         return true;
       }
       debug::log::get(debug::logINDENT, 10) << "FAIL" << std::endl;
@@ -35,16 +35,21 @@ namespace ui
 
   bool ui_manager::create_window(int h, int w, std::string title)
   {
-    return current_ui->create_window(h, w, title);
+    return current_managee->create_window(h, w, title);
   }
 
   bool ui_manager::ui_satisfies_running_condition()
   {
-    return current_ui->satisfies_running_condition();
+    return current_managee->satisfies_running_condition();
   }
 
   void ui_manager::update_ui()
   {
-    current_ui->update_ui();
+    current_managee->update_ui();
+  }
+
+  void ui_manager::set_backend_context_version(int major, int minor)
+  {
+    current_managee->set_backend_context_version(major, minor);
   }
 }
--- src/ui/ui_manager.hh
+++ src/ui/ui_manager.hh
@@ -3,12 +3,13 @@
 # include <memory>
 # include <vector>
 
+# include "../base/manager.hh"
 # include "base_ui.hh"
 # include "uis.hh"
 
 namespace ui
 {
-  class ui_manager
+  class ui_manager : public base::base_manager<base_ui>
   {
     public:
       ui_manager();
@@ -18,9 +19,6 @@ namespace ui
       bool create_window(int h, int w, std::string title);
       bool ui_satisfies_running_condition();
       void update_ui();
-
-    private:
-      std::vector<std::shared_ptr<base_ui>> uis;
-      std::shared_ptr<base_ui> current_ui;
+      void set_backend_context_version(int major, int minor);
   };
 }
