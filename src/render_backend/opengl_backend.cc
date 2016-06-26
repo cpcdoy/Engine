e1c99806fd4842066e36f5f4e0e6ae7ca796f8bb
Chady Dimachkie <dimach_c@epita.fr>
2016-06-26 14:10:46 +0200

    Add: more OpenGL states initialization in the init function of the gl render_backend + Add: generate_compatible_mesh function + Add: generate VAO for the mesh in the gl backend + Add: render function that takes a mesh and transforms it into a gl_mesh (for now)

--- src/render_backend/opengl_backend.cc
+++ src/render_backend/opengl_backend.cc
@@ -20,6 +20,42 @@ namespace render_backend
       debug::log::get(debug::logINDENT) << "You need to call the ui_manager, initialize it and create a window before" << std::endl;
       return false;
     }
+
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+    glEnable(GL_CULL_FACE);
+
+    glCullFace(GL_BACK);
+
     return true;
+  }
+
+  std::shared_ptr<resource::mesh>
+  opengl_backend::generate_compatible_mesh(std::shared_ptr<resource::mesh> mesh)
+  {
+    std::shared_ptr<resource::gl_mesh> gl_mesh = std::make_shared<resource::gl_mesh>(mesh);
+
+    gl_mesh->set_vao(generate_vao());
+
+    return gl_mesh;
+  }
+
+  GLuint opengl_backend::generate_vao()
+  {
+    debug::log::get(debug::logINFO) << "Generating a VAO" << std::endl;
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    debug::log::get(debug::logINDENT) << "VAO : " << vao << std::endl;
+
+    return vao;
+  }
+
+  void opengl_backend::render(std::shared_ptr<resource::mesh> mesh)
+  {
+    auto m = std::static_pointer_cast<resource::gl_mesh>(mesh);
   }
 }
--- src/render_backend/opengl_backend.hh
+++ src/render_backend/opengl_backend.hh
@@ -1,8 +1,11 @@
 #pragma once
 
 # include <GL/glew.h>
+# include <memory>
 
 # include "render_backend.hh"
+# include "../resource/mesh.hh"
+# include "../resource/gl_mesh.hh"
 
 namespace render_backend
 {
@@ -13,5 +16,10 @@ namespace render_backend
       ~opengl_backend();
 
       virtual bool init_backend() override;
+
+      virtual std::shared_ptr<resource::mesh> generate_compatible_mesh(std::shared_ptr<resource::mesh> mesh) override;
+      virtual void render(std::shared_ptr<resource::mesh> mesh) override;
+
+      GLuint generate_vao();
   };
 }
