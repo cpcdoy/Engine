aca461f7b2f892c35c6fde936d842cd5ad4a6910
Chady Dimachkie <dimach_c@epita.fr>
2016-06-29 18:48:59 +0200

    Update: the main test

--- tests/debug/main.cc
+++ tests/debug/main.cc
@@ -14,6 +14,10 @@ int main(void)
 
   mesh m = e.load_mesh("tests/res/test.obj");
   m->set_pos(glm::vec3(0, 1, 0));
+  mesh m2 = e.load_mesh("tests/res/test.obj");
+  m2->set_pos(glm::vec3(0, 1, 2));
+  mesh m3 = e.load_mesh("tests/res/test.obj");
+  m3->set_pos(glm::vec3(0, 1, 4));
   e.load_mesh("res/obj/floor.obj");
   e.load_mesh("lol.obj");
 
@@ -23,7 +27,7 @@ int main(void)
   cam->set_camera_position(glm::vec3(3, 3, 3));
 
   double a = 0;
-  auto f = std::function<void()>([&](){ a-=0.0001; m->set_pos(glm::vec3(a, 0, 0)); });
+  auto f = std::function<void()>([&](){ a-=0.0001; m->set_pos(glm::vec3(a, 0, 0)); m2->set_pos(glm::vec3(a, 0, 0)); m3->set_pos(glm::vec3(a, 0, 0)); });
 
   e.run(f);
 
