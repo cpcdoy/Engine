383b9d7a2440d9d878d74f37882c0f0fb99d104d
Chady <chady.dimachkie@epita.fr>
2016-09-27 16:34:12 +0200

    Add: SFINAE to check if all templates variadic types are equivalent (used for instance in a std::enable_if)

--- src/utils/utils.hh
+++ src/utils/utils.hh
@@ -1,8 +1,20 @@
 #pragma once
 
 # include <string>
+# include <type_traits>
 
 namespace util
 {
   std::string replace_all(std::string str, const std::string& from, const std::string& to);
+
+  template<typename T, typename... Rest>
+      struct all_same : std::false_type {};
+
+  template<typename T, typename First>
+      struct all_same<T, First> : std::is_same<T, First> {};
+
+  template<typename T, typename First, typename... Rest>
+      struct all_same<T, First, Rest...>
+      : std::integral_constant<bool, std::is_same<T, First>::value && all_same<T, Rest...>::value>
+      {};
 }
